76dbe6ccd25b5bc35312015d444c19da9341d627
이우주 <tunerprime@soongsil.ac.kr>
2018-09-09 14:11:55 +0900

    [mod] properties  - 프로퍼티 분리

--- src/main/java/festival/pos/FestivalPosApplication.java
+++ src/main/java/festival/pos/FestivalPosApplication.java
@@ -2,8 +2,10 @@ package festival.pos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = { "classpath:${jdbc.config}" })
 public class FestivalPosApplication {
 
 	public static void main(String[] args) {
